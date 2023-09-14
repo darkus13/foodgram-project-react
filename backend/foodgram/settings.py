a7f4a44b286dc3e418a24f988c564abd4f627e27
Andrej Troshin <darkusss13@ya.ru>
2023-09-14 20:29:57 +0300

    Finalsers

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -60,12 +60,8 @@ WSGI_APPLICATION = "foodgram.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'you_need_to_set_the_password_in_env'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', 5432)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
